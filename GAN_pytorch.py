bc4d7e106675fc9fd97db991cc97968b6a9ea638
Keewon Shin <kevinkwshin@gmail.com>
2019-11-15 09:04:07 +0900

    Update GAN_pytorch.py

--- GAN_pytorch.py
+++ GAN_pytorch.py
@@ -3,3 +3,9 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
+import torchvision.transform as transforms
+
+mb_size = 64
+transform = transforms.Compose((transforms.ToTensor()))
